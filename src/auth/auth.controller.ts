b08b30b86cd4e310d673d86baad35e272a59ac97
Bruno Daniel Rios Cosser <bruno_rioscosser18_2314@hotmail.com>
2023-11-02 02:42:11 -0500

    deploy

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto, LoginDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
+  }
+  @Post('/login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+  @Post('/register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.authService.register(registerUserDto);
+  }
+  @UseGuards(AuthGuard)
+  @Get()
+  findAll() {
+    return this.authService.findAll();
+  }
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken(@Request() req: Request): LoginResponse {
+    const user = req['user'] as User;
+    return {
+      user,
+      token: this.authService.getJwt({ id: user._id }),
+    };
+  }
+}
---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export interface JwtPayLoad {
+  id: string;
+  iat?: number;
+  exp?: number;
+}
